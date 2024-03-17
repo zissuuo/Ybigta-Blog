0827fa396de0b8dd3c654b03a16f1ff313472a43
zissuuo <zissuuo@yonsei.ac.kr>
2024-03-18 00:32:05 +0900

    feat: add redirect homepage route 2

--- frontend/src/components/ui/HeaderComponent.jsx
+++ frontend/src/components/ui/HeaderComponent.jsx
@@ -7,18 +7,44 @@ const Header = styled.div`
   padding-right: 48px;
   display: flex;
   height: 5rem;
-  justify-content: space-between;
+  justify-content: space-between; /* 수정됨 */
   align-items: center;
   border-bottom: 1px solid #d4d4d4;
 `;
 
+/* 우측에 비어있는 박스를 추가하여 균형을 맞춤. */
+const RightBox = styled.div`
+  width: 102px; /* 로고와 동일한 크기로 설정하여 균형을 유지 */
+  height: 40px;
+`;
+
+
 const HeaderLogo = styled.div`
   width: 102px;
   height: 40px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   cursor: pointer;
+`;
+
+const PageBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ClickableBox = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 1.125rem;
+  color: #000000;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-family: 'Pretendard-Medium';
+  &:hover {
+    background-color: #e5e5e5;
+    color: #3b82f6;
+  }
 `;
 
 const ClickableBoxLogo = styled(HeaderLogo)`
@@ -27,19 +53,27 @@ const ClickableBoxLogo = styled(HeaderLogo)`
 
 const HeaderComponent = ({ children }) => {
   const navigate = useNavigate();
-  // const navigateTo = (path) => {
-  //   navigate(path);
-  // };
 
-  const redirectToExternalSite = () => {
+  const redirectToHome = () => {
     window.location.href = 'https://ybigta.org/';
+  };
+
+  const redirectToArchive = () => {
+    window.location.href = 'https://ybigta.gitbook.io/archive/';
   };
 
   return (
     <Header>
-        <ClickableBoxLogo onClick={redirectToExternalSite}>
-          <img src={process.env.PUBLIC_URL + '/ybigta_logo.svg'} style = {{height: "100%"}}/>
-        </ClickableBoxLogo>
+      <ClickableBoxLogo onClick={redirectToHome}>
+        <img src={process.env.PUBLIC_URL + '/ybigta_logo.svg'} style={{ height: "100%" }} alt="logo" />
+      </ClickableBoxLogo>
+      <PageBox>
+        <ClickableBox>About</ClickableBox>
+        <ClickableBox onClick={redirectToArchive}>Archive</ClickableBox>
+        <ClickableBox>Wiki</ClickableBox>
+        <ClickableBox onClick={() => navigate('/')}>Blog</ClickableBox>
+      </PageBox>
+      <RightBox />
     </Header>
   );
 };
